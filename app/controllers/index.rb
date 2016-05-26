f16ea259fdb7ae08ab80d038e24585fb3b91ab69
Ben Goldstein & Ed Mechem & Abid Ramay & Traci <pair+bengoldstein88.edmechem.kasper341.trefong@devbootcamp.com>
2016-05-26 16:15:06 -0700

    implement login, bcrypt

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,47 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/users/new' do
+  erb :registration
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+
+  erb :user_page
+end
+
+get '/login' do
+
+  erb :login
+end
+
+post '/users/new' do
+  @new_user = User.new(params[:user])
+  @new_user.password = params[:password]
+  if @new_user.save
+    redirect "users/#{@new_user.id}"
+  else
+    @error_message = @new_user.errors.messages
+    redirect '/'
+  end
+end
+
+post '/users/new/redirect' do
+  p params[:user_name]
+  # @user = User.find_by(user_name: params[:user_name])
+  @user = User.where(user_name: params[:user_name]).first
+  p @user
+  if @user
+    if @user.password == params[:password]
+      redirect "/users/#{@user.id}"
+    else
+      redirect '/'
+    end
+  else
+    redirect '/'
+  end
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
+  include BCrypt
 
   has_many :followings_as_follower, class_name: 'Following', foreign_key: :follower_id
   has_many :followings_as_followee, class_name: 'Following', foreign_key: :followee_id
   has_many :followers, through: :followings_as_followee
   has_many :followees, through: :followings_as_follower
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
+
+
